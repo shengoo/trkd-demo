fd0952d7ba507b39f013c9a2291fe0d53332b236
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-10-23 13:21:50 +0800

    a

--- app.js
+++ app.js
@@ -44,38 +44,6 @@ app.use(function(err, req, res, next) {
 });
 
 var tokenService = require('./trkd/TokenService');
-Promise.resolve(tokenService.getToken()).then(data => {console.log(data)})
-// tokenService.createToken();
 
-// tokenService.validateToken('14AF8DFFD2DBB3DFF05067B75EA27488BBEBAC031AC1FE0382F1BFBDDC0C8CDC23BB190DE21552FE739B07F2BCC1DF0C1999D2CABF9B8DC742105B8DC166B4F4A48A3EDE83B48748D691A77A8F8FC8A6D5FADC35298A842C2A33A6B1EF6545F2');
-// var obj = {
-//     name: 'trkd-demo'
-// }
-//
-var fs = require('fs');
-// fs.writeFile("./test", JSON.stringify(obj), function(err) {
-//     if(err) {
-//         return console.log(err);
-//     }
-//
-//     console.log("The file was saved!");
-// });
-var dir = './cache';
-
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-// fs.writeFile('./cache/test', 'utf8', function (err,data) {
-//     if (err) {
-//         return console.log(err);
-//     }
-//     console.log("The file was saved!");
-// });
-
-
-var time = new Date("2017-10-17T08:10:37.0142147Z");
-// console.log(Date.now() < time)
-
-// console.log(fs.existsSync('./cache/token'))
 
 module.exports = app;
